b6f08e1ef1ec167cf1b58f6cf5e68143d7d1474f
Hossein Kavand <hskavand@gmail.com>
2024-04-16 21:07:27 +0330

    update the gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import viteTsconfigPaths from 'vite-tsconfig-paths';
+import viteTsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), viteTsconfigPaths()],
-  build: {
-    outDir: "build",
-  },
+  base: "/test-interview/",
 });
